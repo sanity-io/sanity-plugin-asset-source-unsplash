b9000ed560633d60c181132c0d9b7b55e84cbcd9
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2019-11-12 16:05:32 +0100

    Warn about missing configuration

--- src/components/UnsplashAssetSource.tsx
+++ src/components/UnsplashAssetSource.tsx
@@ -34,15 +34,28 @@ export default class UnsplashAssetSource extends React.Component<Props> {
     this.props.onClose()
   }
 
+  renderConfigWarning() {
+    return (
+      <div>
+        <h2>Missing configuration</h2>
+        <p>You must first configure the plugin with your Unsplash API key</p>
+        <p>Edit the <code>./config/asset-source-unsplash.json</code> file in your Sanity Studio folder.</p>
+      </div>
+    )
+  }
+
   render() {
     return (
       <Dialog title="Select image from Unsplash" onClose={this.handleClose} isOpen>
         <div className={styles.root}>
-          <UnsplashReact
-            accessKey={pluginConfig.accessKey}
-            Uploader={withDefaultProps(ShallowUploader, {})}
-            onFinishedUploading={this.handleSelect}
-          />
+          {pluginConfig.accessKey && (
+            <UnsplashReact
+              accessKey={pluginConfig.accessKey}
+              Uploader={withDefaultProps(ShallowUploader, {})}
+              onFinishedUploading={this.handleSelect}
+            />
+          )}
+        {!pluginConfig.accessKey && this.renderConfigWarning()}
         </div>
       </Dialog>
     )
