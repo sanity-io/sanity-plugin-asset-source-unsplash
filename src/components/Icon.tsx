0c0b241e4ffdcdabe77a4dd807919c6fb1c8195c
Cody Olsen <stipsan@gmail.com>
2022-08-17 06:49:37 +0000

    fix: use currentColor in Unsplash logo svg

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Svg = styled.svg`
+  transform: scale(0.8);
+`
 
 export default function UnsplashIcon() {
   return (
-    <svg role="img" viewBox="0 0 24 24" width="1em" height="1em">
+    <Svg role="img" viewBox="0 0 24 24" width="1em" height="1em" fill="currentColor">
       <title />
       <path d="M7.5 6.75V0h9v6.75h-9zm9 3.75H24V24H0V10.5h7.5v6.75h9V10.5z" />
-    </svg>
+    </Svg>
   )
 }
