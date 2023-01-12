d10e5324e3b8eef5eebd8fe03326bb7f748c9cb3
Cody Olsen <stipsan@gmail.com>
2023-01-12 02:51:49 +0100

    fix: adjust unsplash logo negative space with `viewBox` instead of CSS `transform: scale`
    Signed-off-by: Cody Olsen <stipsan@gmail.com>

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,16 +1,8 @@
-import React from 'react'
-import styled from 'styled-components'
-
-const Svg = styled.svg`
-  transform: scale(0.8);
-  transform-origin: top;
-`
-
 export default function UnsplashIcon() {
   return (
-    <Svg role="img" viewBox="0 0 24 24" width="1em" height="1em" fill="currentColor">
+    <svg role="img" viewBox="-4 -4 28 28" width="1em" height="1em" fill="currentColor">
       <title />
       <path d="M7.5 6.75V0h9v6.75h-9zm9 3.75H24V24H0V10.5h7.5v6.75h9V10.5z" />
-    </Svg>
+    </svg>
   )
 }
